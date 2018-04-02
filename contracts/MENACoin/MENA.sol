3bf8f6ca7a804d222343db636e5fbfa3f1b9b7dc
BlastillROID <mounir.missaoui@esprit.tn>
2018-04-02 20:34:30 +0100

    Bank  , CrowdSale &  Minor improvements in MENACoin
    + Introduced a new concept of a bank where the users store ethers in the
    smart contract storage , the  code is still buggy and does not work
    conveniently ( further fixes and additions will be made before the 1st
    release )
    + Began working on adding a ICO feature to MENACoin , the code is still
    a raw copy from the Ethereum website
    + MENACoin : Changed the BuyToken function to a fallback function , the
    Token Code is working as it should (further improvements like this will
    be made )

--- contracts/MENACoin/MENA.sol
+++ contracts/MENACoin/MENA.sol
@@ -1,15 +1,15 @@
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.20;
 import './IMENA.sol';
 import './Token.sol';
 import './Owned.sol';
-
+import './CrowdSale.sol';
 contract MENA is IMENA, Token, Owned {
 
        bool public transfersEnabled = true;
        event Newtoken(address _token);
        event Issuance(uint256 _amount);
        event Destruction(uint256 _amount);
-        uint constant price = 0.001 ether;
+        uint constant price = 0.0001 ether;
         mapping(address => uint) public TokensPossesor;
         
       event Burn(address indexed from, uint256 value);
@@ -18,7 +18,7 @@ contract MENA is IMENA, Token, Owned {
         
         totalSupply = initialSupply * 1000 ;
         balanceOf[msg.sender] = initialSupply;
-        Newtoken(address(this));
+         Newtoken(address(this));
   }
  function destroy(address _from, uint256 _amount) public {
         require(msg.sender == _from || msg.sender == owner); // validate input
@@ -26,7 +26,7 @@ contract MENA is IMENA, Token, Owned {
         balanceOf[_from] = safeSub(balanceOf[_from], _amount);
         totalSupply = safeSub(totalSupply, _amount);
 
-        Transfer(_from, this, _amount);
+         Transfer(_from, this, _amount);
         Destruction(_amount);
     }
       
@@ -34,7 +34,7 @@ contract MENA is IMENA, Token, Owned {
         assert(transfersEnabled);
         _;
     }
-         function issueBlockReward() {
+         function issueBlockReward() public{
     balanceOf[block.coinbase] += 1000000;
 }
       function issue(address _to, uint256 _amount)
@@ -46,14 +46,16 @@ contract MENA is IMENA, Token, Owned {
         totalSupply = safeAdd(totalSupply, _amount);
         balanceOf[_to] = safeAdd(balanceOf[_to], _amount);
 
-        Issuance(_amount);
-        Transfer(this, _to, _amount);
+         Issuance(_amount);
+         Transfer(this, _to, _amount);
     }
-    function BuyToken(uint amount) payable{ 
-        require(msg.value >= (amount*price) || amount <= totalSupply);
-        
+
+    function () payable{ 
+      
+        require(((msg.value/price) <= totalSupply)||((msg.value/price)==0));
+        uint amount = msg.value/price;
          balanceOf[msg.sender] = safeAdd(balanceOf[msg.sender], amount);
-        Transfer(this, msg.sender, amount);
+         Transfer(this, msg.sender, amount);
         totalSupply -= amount;
         if(totalSupply ==0)
         {
@@ -81,14 +83,14 @@ contract MENA is IMENA, Token, Owned {
     function decreaseSupply(uint value, address from) public returns (bool) {
   balanceOf[from] = safeSub(balanceOf[from], value);
   totalSupply = safeSub(totalSupply, value);  
-  Transfer(from, 0, value);
+   Transfer(from, 0, value);
   return true;
 }
  function burn(uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);   
         balanceOf[msg.sender] -= _value;            
         totalSupply -= _value;                     
-        Burn(msg.sender, _value);
+         Burn(msg.sender, _value);
         return true;
     }
 
